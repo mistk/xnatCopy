a647aa95a87ff7ce164004142cf67c3aa0893622
hubert <xnatural@msn.cn>
2017-01-22 15:18:43 +0800

    add spring boot jsp support.

--- src/main/java/my/controller/TestController.java
+++ src/main/java/my/controller/TestController.java
@@ -1,8 +1,13 @@
 package my.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("test")
 public class TestController {
-
+    @RequestMapping("testjsp")
+    String index() {
+        return "index";
+    }
 }
